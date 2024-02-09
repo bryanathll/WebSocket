3bdeae4be1f8142971120f401c3f37e2f979dbbe
bryanathll <bryannatanael77@gmail.com>
2024-02-10 04:42:13 +0700

    adding app

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
- const socket = io('ws://localhost:8080');
+const socket = io('ws://localhost:8080');
 
 //  listen for message
 socket.on('message', text =>{
     const el = document.createElement('li');
     el.innerHTML = text;
-    document.querySelector('ul').appendChild('el')
+    document.querySelector('ul').appendChild(el)
 });
 
 document.querySelector('button').onclick = () => {
